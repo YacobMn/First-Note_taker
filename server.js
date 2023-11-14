1c5e4c6a9afc79e1f99cb9a25a344d49a1fba7fe
unknown <yacobabr@gmail.com>
2023-11-14 17:49:08 -0600

    notetaker

--- server.js
+++ server.js
@@ -1,5 +1,28 @@
 const express = require('express');
 const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+uuidv4(); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
 
 const app = express(); // done
-const PORT = 3001;
+
+const PORT = process.env.PORT || 3001;
+
+// middle ware 
+// routes 
+// app.listen
+// 11 -14 middle ware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
